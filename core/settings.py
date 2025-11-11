c4b81616850b38e3156c26db9a08d8a7a49c4e66
Juliano Roque Vieira <julianovieira@yahoo.com.br>
2025-11-11 14:50:18 -0300

    Alteracoes

--- core/settings.py
+++ core/settings.py
@@ -110,19 +110,22 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
-
+LANGUAGE_CODE = "pt-br"
+TIME_ZONE = "America/Sao_Paulo"
 USE_I18N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+# Diretórios onde o Django procura por arquivos estáticos
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+# Diretório onde os arquivos estáticos são coletados para produção
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
